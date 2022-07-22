c931e102505d1387fb268d6cdbcf4f0d4a6d30a2
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2022-07-22 02:45:49 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,480 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from tqdm import tqdm_notebook
+import plotly.figure_factory as ff
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import warnings
+warnings.filterwarnings('ignore')
+
+plt.style.use('fivethirtyeight')
+%matplotlib inline
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+        
+data.describe()
+
+data.info()
+
+print('There are {} data points and {} features in the data'.format(data.shape[0],data.shape[1]))
+
+sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis')  #visualization missing data
+
+#calc null values
+for i in data.columns:
+    if data[i].isnull().sum()>0:
+        print("There are {} null values in {} column".format(data[i].isnull().sum(),i))
+
+#Handling Null Values
+#PH
+data['ph'].describe()
+
+#Filling the missing values by mean
+
+data['ph_mean']=data['ph'].fillna(data['ph'].mean())
+data['ph_mean'].isnull().sum()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+data['ph'].plot(kind='kde', ax=ax)
+data.ph_mean.plot(kind='kde', ax=ax, color='red')
+lines, labels = ax.get_legend_handles_labels()
+ax.legend(lines, labels, loc='best')
+plt.show()
+
+def impute_nan(df,variable):
+    df[variable+"_random"]=df[variable]
+    ##It will have the random sample to fill the na
+    random_sample=df[variable].dropna().sample(df[variable].isnull().sum(),random_state=0)
+    ##pandas need to have same index in order to merge the dataset
+    random_sample.index=df[df[variable].isnull()].index
+    df.loc[df[variable].isnull(),variable+'_random']=random_sample
+    
+impute_nan(data,"ph") #using pandas represent null values
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+data['ph'].plot(kind='kde', ax=ax)
+data.ph_random.plot(kind='kde', ax=ax, color='green')
+data.ph_mean.plot(kind='kde', ax=ax, color='red')
+lines, labels = ax.get_legend_handles_labels()
+ax.legend(lines, labels, loc='best')
+plt.show()
+
+impute_nan(data,"Sulfate")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+data['Sulfate'].plot(kind='kde', ax=ax)
+data["Sulfate_random"].plot(kind='kde', ax=ax, color='green')
+#data.ph_mean.plot(kind='kde', ax=ax, color='red')
+lines, labels = ax.get_legend_handles_labels()
+ax.legend(lines, labels, loc='best')
+plt.show()
+
+impute_nan(data,"Trihalomethanes")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+data['Trihalomethanes'].plot(kind='kde', ax=ax)
+data.Trihalomethanes_random.plot(kind='kde', ax=ax, color='green')
+lines, labels = ax.get_legend_handles_labels()
+ax.legend(lines, labels, loc='best')
+plt.show()
+
+data=data.drop(['ph','Sulfate','Trihalomethanes','ph_mean'],axis=1)
+
+data.isnull().sum()
+
+#check for correlation
+plt.figure(figsize=(20, 17))
+matrix = np.triu(data.corr())
+sns.heatmap(data.corr(), annot=True,linewidth=.8, mask=matrix, cmap="rocket",cbar=False);
+
+#There are no correlated columns presebt in the data
+
+sns.pairplot(data, hue="Potability", palette="husl");
+
+def distributionPlot(data):
+    """ 
+    Creates distribution plot.
+    """
+    fig = plt.figure(figsize=(20, 20))
+    for i in tqdm_notebook(range(0, len(data.columns))):
+        fig.add_subplot(np.ceil(len(data.columns)/3), 3, i+1)
+        sns.distplot(
+            data.iloc[:, i], color="lightcoral", rug=True)
+        fig.tight_layout(pad=3.0)
+plot_data = data.drop(['Potability'], axis =1)
+distributionPlot(plot_data)
+
+#Hardness
+data['Hardness'].describe()
+
+plt.figure(figsize = (16, 7))
+sns.distplot(data['Hardness'])
+plt.title('Distribution Plot of Hardness\n', fontsize =  20)
+plt.show()
+
+# basic scatter plot
+fig = px.scatter(data,range(data['Hardness'].count()), sorted(data['Hardness']),
+                 color=data['Potability'],
+                 labels={
+                     'x': "Count",
+                     'y': "Hardness",
+                     'color':'Potability'
+                     
+                 }, template = 'plotly_dark')
+fig.update_layout(title='Hardness wrt Potability')
+fig.show()
+
+px.histogram(data_frame = data, x = 'Hardness', nbins = 10, color = 'Potability', marginal = 'box',
+             template = 'plotly_dark')
+             
+ data['Solids'].describe()
+
+plt.figure(figsize = (16, 7))
+sns.distplot(data['Solids'])
+plt.title('Distribution Plot of Solids\n', fontsize =  20)
+plt.show()
+
+
+fig = px.scatter(data, sorted(data["Solids"]), range(data["Solids"].count()), color="Potability", facet_col="Potability", 
+                 facet_row="Potability")
+fig.show()
+
+px.histogram(data_frame = data, x = 'Solids', nbins = 10, color = 'Potability', marginal = 'box',
+             template = 'plotly_dark')
+             
+# basic scatter plot
+fig = px.scatter(data,range(data['Solids'].count()), sorted(data['Solids']),
+                 color=data['Potability'],
+                 labels={
+                     'x': "Count",
+                     'y': "Hardness",
+                     'color':'Potability'
+                     
+                 },
+                 color_continuous_scale=px.colors.sequential.tempo,
+                 template = 'plotly_dark')
+fig.update_layout(title='Hardness wrt Potability')
+fig.show()
+
+data['Chloramines'].describe()
+
+plt.figure(figsize = (16, 7))
+sns.distplot(data['Chloramines'])
+plt.title('Distribution Plot of Chloramines\n', fontsize =  20)
+plt.show()
+
+fig = px.line(x=range(data['Chloramines'].count()), y=sorted(data['Chloramines']),color=data['Potability'], labels={
+                     'x': "Count",
+                     'y': "Chloramines",
+                     'color':'Potability'
+                     
+                 }, template = 'plotly_dark')
+fig.update_layout(title='Chloramines wrt Potability')
+fig.show()
+
+fig = px.box(x = 'Chloramines', data_frame = data, template = 'plotly_dark')
+fig.update_layout(title='Chloramines')
+fig.show()
+
+data["Conductivity"].describe()
+
+plt.figure(figsize = (16, 7))
+sns.distplot(data['Conductivity'])
+plt.title('Distribution Plot of Conductivity\n', fontsize =  20)
+plt.show()
+
+fig = px.bar(data, x=range(data['Conductivity'].count()),
+             y=sorted(data['Conductivity']), labels={
+                     'x': "Count",
+                     'y': "Conductivity",
+                     'color':'Potability'
+                     
+                 },
+             color=data['Potability']
+             ,template = 'plotly_dark')
+fig.update_layout(title='Conductivity wrt Potability')
+fig.show() 
+group_labels = ['distplot'] # name of the dataset
+
+fig = ff.create_distplot([data['Conductivity']], group_labels)
+fig.show()
+
+data['Organic_carbon'].describe()
+
+group_labels = ['Organic_carbon'] # name of the dataset
+
+fig = ff.create_distplot([data['Organic_carbon']], group_labels)
+fig.show()
+
+sns.catplot(x = 'Organic_carbon', y = 'Organic_carbon', hue = 'Potability', data = data, kind = 'box',
+            height = 5, aspect = 2)
+plt.show()
+
+data['Turbidity'].describe()
+
+group_labels = ['Turbidity'] # name of the dataset
+
+fig = ff.create_distplot([data['Turbidity']], group_labels)
+fig.show()
+
+data['turbid_class']=data['Turbidity'].astype(int)
+
+data['turbid_class'].unique()
+
+px.scatter(data_frame = data, x = 'Turbidity', y = 'turbid_class', color = 'Potability', template = 'plotly_dark')
+fig = px.pie(data,
+             values=data['turbid_class'].value_counts(), 
+             names=data['turbid_class'].value_counts().keys(),
+            )
+fig.update_layout(
+    title='turbid_class',
+    template = 'plotly_dark'
+)
+fig.show()
+data=data.drop(['turbid_class'],axis=1)
+
+data['ph_random'].describe()
+
+group_labels = ['ph_random'] # name of the dataset
+
+fig = ff.create_distplot([data['ph_random']], group_labels)
+fig.show()
+
+px.histogram(data_frame = data, x = 'ph_random', nbins = 10, color = 'Potability', marginal = 'box',
+             template = 'plotly_dark')
+             
+fig = px.scatter(data, sorted(data["ph_random"]), range(data["ph_random"].count()), color="Potability", facet_col="Potability", 
+                 facet_row="Potability")
+fig.show()
+
+data['Sulfate_random'].describe()
+
+group_labels = ['distplot'] # name of the dataset
+
+fig = ff.create_distplot([data['Sulfate_random']], group_labels)
+fig.show()
+
+sns.catplot(x = 'Sulfate_random', y = 'Sulfate_random', hue = 'Potability', data = data, kind = 'box',
+            height = 5, aspect = 2)
+plt.show()
+
+data['Trihalomethanes_random'].describe()
+
+
+group_labels = ['Trihalomethanes_random'] # name of the dataset
+
+fig = ff.create_distplot([data['Trihalomethanes_random']], group_labels)
+fig.show()
+
+fig = px.box(x = 'Trihalomethanes_random', data_frame = data, template = 'plotly_dark')
+fig.update_layout(title='Trihalomethanes_random')
+fig.show()
+
+fig = px.line(x=range(data['Trihalomethanes_random'].count()), y=sorted(data['Trihalomethanes_random']),color=data['Potability'], labels={
+                     'x': "Count",
+                     'y': "Trihalomethanes",
+                     'color':'Potability'
+                     
+                 }, template = 'plotly_dark')
+fig.update_layout(title='Trihalomethane wrt Potability')
+fig.show()
+
+data['Potability'].describe()
+
+px.histogram(data_frame = data, x = 'Potability', color = 'Potability', marginal = 'box',
+             template = 'plotly_dark')
+fig = px.pie(data,
+             values=data['Potability'].value_counts(), 
+             names=data['Potability'].value_counts().keys(),
+            )
+fig.update_layout(
+    title='Potability',
+    template = 'plotly_dark'
+)
+fig.show()
+
+#Data Preprocessing
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+X=data.drop(['Potability'],axis=1)
+y=data['Potability']
+
+scaler = StandardScaler()
+x=scaler.fit_transform(X)
+
+# split the data to train and test set
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.85,random_state=42)
+
+
+print("training data shape:-{} labels{} ".format(x_train.shape,y_train.shape))
+print("testing data shape:-{} labels{} ".format(x_test.shape,y_test.shape))
+
+#Modeling
+#Logistic Regression
+
+from sklearn.linear_model import LogisticRegression
+log = LogisticRegression(random_state=0).fit(x_train, y_train)
+log.score(x_test, y_test)
+
+
+#  Confusion matrix
+from sklearn.metrics import confusion_matrix
+# Make Predictions
+pred1=log.predict(np.array(x_test))
+plt.title("Confusion Matrix testing data")
+sns.heatmap(confusion_matrix(y_test,pred1),annot=True,cbar=False)
+plt.legend()
+plt.show()
+
+
+#K Nearest Neighbours
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=2)
+# Train the model using the training sets
+knn.fit(x_train,y_train)
+
+#Predict Output
+predicted= knn.predict(x_test) # 0:Overcast, 2:Mild
+#  Confusion matrix
+from sklearn.metrics import confusion_matrix
+
+# Make Predictions
+pred1=knn.predict(np.array(x_test))
+plt.title("Confusion Matrix testing data")
+sns.heatmap(confusion_matrix(y_test,pred1),annot=True,cbar=False)
+plt.legend()
+plt.show()
+
+#SVM
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+svmc = svm.SVC()
+svmc.fit(x_train, y_train)
+
+y_pred = svmc.predict(x_test)
+print(accuracy_score(y_test,y_pred))
+0.6808943089430894
+
+#  Confusion matrix
+from sklearn.metrics import confusion_matrix
+
+# Make Predictions
+pred1=svmc.predict(np.array(x_test))
+plt.title("Confusion Matrix testing data")
+sns.heatmap(confusion_matrix(y_test,pred1),annot=True,cbar=False)
+plt.legend()
+plt.show()
+
+#Decision Tree
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+tre = tree.DecisionTreeClassifier()
+tre = tre.fit(x_train, y_train)
+
+y_pred = tre.predict(x_test)
+print(accuracy_score(y_test,y_pred))
+
+#  Confusion matrix
+from sklearn.metrics import confusion_matrix
+
+# Make Predictions
+pred1=tre.predict(np.array(x_test))
+plt.title("Confusion Matrix testing data")
+sns.heatmap(confusion_matrix(y_test,pred1),annot=True,cbar=False)
+plt.legend()
+plt.show()
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+# create the model
+model_rf = RandomForestClassifier(n_estimators=500, oob_score=True, random_state=100)
+
+
+# fitting the model
+model_rf=model_rf.fit(x_train, y_train) 
+
+y_pred = model_rf.predict(x_test)
+print(accuracy_score(y_test,y_pred))
+
+#  Confusion matrix
+from sklearn.metrics import confusion_matrix
+# Make Predictions
+pred1=model_rf.predict(np.array(x_test))
+plt.title("Confusion Matrix testing data")
+sns.heatmap(confusion_matrix(y_test,pred1),annot=True,cbar=False)
+plt.legend()
+plt.show()
+
+
+#XG Boost
+from xgboost import XGBClassifier
+from sklearn.metrics import r2_score
+
+xgb = XGBClassifier(colsample_bylevel= 0.9,
+                    colsample_bytree = 0.8, 
+                    gamma=0.99,
+                    max_depth= 5,
+                    min_child_weight= 1,
+                    n_estimators= 8,
+                    nthread= 5,
+                    random_state= 0,
+                    )
+xgb.fit(x_train,y_train)
+
+print('Accuracy of XGBoost classifier on training set: {:.2f}'
+     .format(xgb.score(x_train, y_train)))
+print('Accuracy of XGBoost classifier on test set: {:.2f}'
+     .format(xgb.score(x_test, y_test)))
+
+
+from sklearn.metrics import confusion_matrix
+
+conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+plt.figure(figsize = (15, 8))
+sns.set(font_scale=1.4) # for label size
+sns.heatmap(conf_matrix, annot=True, annot_kws={"size": 16},cbar=False, linewidths = 1) # font size
+plt.title("Test Confusion Matrix")
+plt.xlabel("Predicted class")
+plt.ylabel("Actual class")
+plt.savefig('conf_test.png')
+plt.show()
+
+#SVM tuned
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV  #to select the best values for parametres
+svc=SVC()
+param_grid={'C':[1.2,1.5,2.2,3.5,3.2,4.1],'kernel':['linear', 'poly', 'rbf', 'sigmoid'],'degree':[1,2,4,8,10],'gamma':['scale','auto']}
+gridsearch=GridSearchCV(svc,param_grid=param_grid,n_jobs=-1,verbose=4,cv=3)
+gridsearch.fit(x_train,y_train)
+
+
+y_pred=gridsearch.predict(x_test)
+from sklearn.metrics import confusion_matrix
+
+conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+plt.figure(figsize = (15, 8))
+sns.set(font_scale=1.4) # for label size
+sns.heatmap(conf_matrix, annot=True, annot_kws={"size": 16},cbar=False, linewidths = 1) # font size
+plt.title("Test Confusion Matrix")
+plt.xlabel("Predicted class")
+plt.ylabel("Actual class")
+plt.savefig('conf_test.png')
+plt.show()
